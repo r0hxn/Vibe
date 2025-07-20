89a3b81ef4387efb361005454c954e5e84603432
r-h-n <hellorohanpandey@gmail.com>
2025-07-20 22:42:50 +0530

    Add message UI integration and logic

--- src/modules/projects/server/procedures.ts
+++ src/modules/projects/server/procedures.ts
@@ -3,8 +3,32 @@ import { inngest } from "@/inngest/client";
 import { z } from "zod";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import { generateSlug } from "random-word-slugs";
+import { TRPCError } from "@trpc/server";
 
 export const projectsRouter = createTRPCRouter({
+  getOne: baseProcedure
+    .input(
+      z.object({
+        id: z.string().min(1, { message: "ID is required" }),
+      })
+    )
+    .query(async ({ input }) => {
+    const existingProject = await prisma.project.findUnique({
+      where: {
+        id: input.id,
+      },
+    });
+
+    if(!existingProject){
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: "Project not found",
+      });
+    }
+
+    return existingProject;
+  }),
+
   getMany: baseProcedure.query(async () => {
     const projects = await prisma.project.findMany({
       orderBy: {
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,6 +1,6 @@
-import { projectsRouter } from '@/modules/projects/procedures';
+import { projectsRouter } from '@/modules/projects/server/procedures';
 import { createTRPCRouter } from '../init';
-import { messagesRouter } from '@/modules/messages/procedures';
+import { messagesRouter } from '@/modules/messages/server/procedures';
 
 export const appRouter = createTRPCRouter({
     messages: messagesRouter,
