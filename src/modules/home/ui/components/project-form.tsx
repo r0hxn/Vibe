39ed155bdb74c2c398ddb83990ddf31e7774e29e
pranayk013 <pranaykhandelwal001@gmail.com>
2025-07-24 03:01:39 +0530

    Implement authentication system

--- src/modules/home/ui/components/project-form.tsx
+++ src/modules/home/ui/components/project-form.tsx
@@ -14,6 +14,7 @@ import { Button } from "@/components/ui/button";
 import { Form, FormField } from "@/components/ui/form";
 import { useRouter } from "next/navigation";
 import { PROJECT_TEMPLATES } from "@/modules/home/constants";
+import { useClerk } from "@clerk/nextjs";
 
 const formSchema = z.object({
     value: z.string()
@@ -24,6 +25,7 @@ const formSchema = z.object({
 export const ProjectForm = () => {
     const router = useRouter();
     const trpc = useTRPC();
+    const clerk = useClerk();
     const queryClient = useQueryClient();
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -41,8 +43,12 @@ export const ProjectForm = () => {
             // TODO invalidate usage status
         },
         onError: (error) => {
-            // Todo redirect to pricing page if specific error
             toast.error(error.message);
+            if (error.data?.code === "UNAUTHORIZED") {
+                clerk.openSignIn();
+            }
+
+            // Todo redirect to pricing page if specific error
         },
     }));
 
